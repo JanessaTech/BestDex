4b3d835f3688e086febf2773e407da430ccbe2b1
Janessa <lvlajzhao1983@gmail.com>
2024-07-29 22:35:18 +0800

    fix bug for menu layout

--- frontend/best-dex/src/components/menu/mobile-menu.tsx
+++ frontend/best-dex/src/components/menu/mobile-menu.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Menu from './menu'
+import { MenuType } from '@/lib/types'
 
 type MobileMenuProps = {
     show: boolean,
@@ -8,14 +10,15 @@ type MobileMenuProps = {
 const MobileMenu: React.FC<MobileMenuProps> = ({show, setShow}) => {
     return (
         <div className={`fixed top-0 left-0 w-full h-screen bg-black ${show ? '': 'hidden'}`}>
-            <div className='m-8'>
-                <div className='w-[32px] h-[32px] rounded-full
-                hover:bg-zinc-500 active:bg-zinc-400
-                flex justify-center items-center cursor-pointer' onClick={() => setShow(false)}>
-                    <img src="/imgs/close.svg" alt="close" />
+            <div className='m-8 flex flex-col'>
+                <div className='flex justify-end'>
+                    <div className='w-[32px] h-[32px] rounded-full
+                    hover:bg-zinc-500 active:bg-zinc-400
+                    flex justify-center items-center cursor-pointer' onClick={() => setShow(false)}>
+                        <img src="/imgs/close.svg" alt="close" />
+                    </div>
                 </div>
-                <div className='bg-lime-200 h-20'>
-                </div>               
+                <Menu menuType={MenuType.MobileMenu} />             
             </div>
         </div>
     )
