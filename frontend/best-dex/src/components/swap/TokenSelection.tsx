379b98040740961003fb9557bb1def48d4651b41
Janessa <lvlajzhao1983@gmail.com>
2024-07-31 20:40:13 +0800

    layout for token selection

--- frontend/best-dex/src/components/swap/TokenSelection.tsx
+++ frontend/best-dex/src/components/swap/TokenSelection.tsx
@@ -1,21 +1,25 @@
 import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-  } from "@/components/ui/popover"
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogDescription,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
 import Arrow from "@/lib/svgs/Arrow"
+import * as VisuallyHidden from '@radix-ui/react-visually-hidden';
 import TokenSelect from "../token/TokenSelect"
 import type { TokenType } from "@/lib/types"
 
-type FromTokenProps = {
+type TokenProps = {
     token?: TokenType,
     handleTokenChange: () => void
 }
 
-const TokenSelection: React.FC<FromTokenProps> = ({token, handleTokenChange}) => {
+const TokenSelection: React.FC<TokenProps> = ({token, handleTokenChange}) => {
     return (
-        <Popover>
-            <PopoverTrigger>
+        <Dialog>
+            <DialogTrigger>
                 <div className={`h-[60px] w-[150px] rounded-s-lg border-2 border-zinc-500 
                                 flex items-center justify-between cursor-pointer px-2
                                 border-e-0
@@ -29,12 +33,22 @@ const TokenSelection: React.FC<FromTokenProps> = ({token, handleTokenChange}) =>
                     }
                     <Arrow className={`h-[15px] w-[15px]
                                     ${token ? 'text-black hover:text-sky-700' : 'text-white'}`}/>
-                </div>
-            </PopoverTrigger>
-            <PopoverContent align="end">
+                </div> 
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle className="text-black">Edit profile</DialogTitle>
+                    <DialogDescription>
+                        Make changes to your profile here. Click save when you're done.
+                    </DialogDescription>
+                </DialogHeader>
+                <VisuallyHidden.Root>
+                    <DialogTitle/>
+                    <DialogDescription/>
+                </VisuallyHidden.Root>
                 <TokenSelect />
-            </PopoverContent>
-        </Popover>
+            </DialogContent>
+        </Dialog>
     )
 }
 
--- frontend/best-dex/src/components/token/TokenSelect.tsx
+++ frontend/best-dex/src/components/token/TokenSelect.tsx
@@ -2,7 +2,7 @@ type TokenSelectProps = {}
 
 const TokenSelect: React.FC<TokenSelectProps> = () => {
     return (
-        <div>
+        <div className="bg-red-300 w-full h-[300px] my-4">
             token select
         </div>
     )
