ea4558906942720b9e17f6ee643b863b67ef734d
Janessa <lvlajzhao1983@gmail.com>
2024-09-11 21:05:09 +0800

    update UTs

--- hardhat/README.md
+++ hardhat/README.md
@@ -18,4 +18,5 @@ test:
     npx hardhat node --fork https://eth-mainnet.g.alchemy.com/v2/lFKEWE2Z7nkAXL73NSeAM2d5EbndwoQk
     npx hardhat test --network localhost .\test\liquidity.test.js
     npx hardhat test --network localhost .\test\SimpleSwap.test.js
+    npx hardhat test --network localhost .\test\CoreSwap.test.js
 ```
---
+++ hardhat/contracts/CoreSwap.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity =0.7.6;
+pragma abicoder v2;
+
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import "hardhat/console.sol";
+
+contract CoreSwap {
+    ISwapRouter public immutable swapRouter;
+    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    IUniswapV3Factory public uniswapV3Factory = IUniswapV3Factory(0x1F98431c8aD98523631AE4a59f267346ea31F984);
+    uint24 public constant feeTier = 3000;
+
+	constructor(ISwapRouter _swapRouter) {
+        swapRouter = _swapRouter;
+    }
+    
+    function swapExactInputSingle(uint amountIn) external returns (uint256 amountOut) {
+	    // Transfer the specified amount of WETH9 to this contract.
+        TransferHelper.safeTransferFrom(WETH9, msg.sender, address(this), amountIn);
+        // Approve the router to spend WETH9.
+        TransferHelper.safeApprove(WETH9, address(swapRouter), amountIn);
+        ISwapRouter.ExactInputSingleParams memory params =
+        ISwapRouter.ExactInputSingleParams({
+          tokenIn: WETH9,
+          tokenOut: DAI,
+          fee: feeTier,
+          recipient: msg.sender,
+          deadline: block.timestamp,
+          amountIn: amountIn,
+          amountOutMinimum: 0,
+          sqrtPriceLimitX96: 0
+        });
+        // The call to `exactInputSingle` executes the swap.
+        amountOut = swapRouter.exactInputSingle(params);
+        return amountOut;
+	}
+
+  function swapExactOutputSingle(uint256 amountOut, uint256 amountInMaximum) external returns (uint256 amountIn) {
+    TransferHelper.safeTransferFrom(DAI, msg.sender, address(this), amountInMaximum);
+    TransferHelper.safeApprove(DAI, address(swapRouter), amountInMaximum);
+    ISwapRouter.ExactOutputSingleParams memory params =
+            ISwapRouter.ExactOutputSingleParams({
+                tokenIn: DAI,
+                tokenOut: WETH9,
+                fee: feeTier,
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountOut: amountOut,
+                amountInMaximum: amountInMaximum,
+                sqrtPriceLimitX96: 0
+            });
+    amountIn = swapRouter.exactOutputSingle(params);
+    console.log('amountIn = ', amountIn);
+    if (amountIn < amountInMaximum) {
+            TransferHelper.safeApprove(DAI, address(swapRouter), 0);
+            TransferHelper.safeTransfer(DAI, msg.sender, amountInMaximum - amountIn);
+    }
+  }
+  
+  function getPool(address _tokenA, address _tokenB, uint24 _fee) external view returns (address pool) {
+        address _pool = uniswapV3Factory.getPool(_tokenA, _tokenB, _fee);
+        console.log("pool:", _pool);
+        return _pool;
+  }
+}
