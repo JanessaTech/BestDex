03d23b38ec72ed06b052617ceba17a80ff595aa4
Janessa <lvlajzhao1983@gmail.com>
2025-06-06 18:50:49 +0800

    refactor codes

--- frontend-wagmi/src/app/page.tsx
+++ frontend-wagmi/src/app/page.tsx
@@ -1,14 +1,9 @@
-import { Providers } from "@/components/providers";
-import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 export default function Home() {
  
   return (
     <div>
-      new dex. Janessa, win, win, win
-        <Providers>
-          <ConnectButton accountStatus='address'/>
-        </Providers>
+        Janesssssa
     </div>
   );
 }
