daca4fd6efecba251f5e6912f2044bd645765753
Janessa <lvlajzhao1983@gmail.com>
2025-06-08 20:33:16 +0800

    add middleware to protect pages

--- frontend-wagmi/src/middleware.ts
+++ frontend-wagmi/src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
     const token = request.cookies.get('token')
-    const protectedRoutes = ['/dashboard']
+    const protectedRoutes = ['/profile']
     console.log('middleware, token', token)
     
     if (protectedRoutes.some(path => 
