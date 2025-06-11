0258104308b0a696c1873d347e12cf9792085143
Janessa <lvlajzhao1983@gmail.com>
2025-06-11 11:15:45 +0800

    layput for swap home

--- frontend-wagmi/src/components/swap/SwapHome.tsx
+++ frontend-wagmi/src/components/swap/SwapHome.tsx
@@ -7,7 +7,10 @@ const SwapHome: React.FC<SwapHomeProps> = () => {;
     const { chains, switchChain } = useSwitchChain()
 
     return (
-        <div className='bg-zinc-900 w-full md:w-[500px] h-80 rounded-xl mx-auto mt-20 p-10'>
+        <div>
+            <div className='font-semibold text-xl my-10 md:hidden'>Swap</div>
+            <div className='bg-zinc-900 w-full md:w-[500px] h-80 rounded-xl md:mt-10 mx-auto p-10'>
+            </div>    
         </div>
     )
 }
