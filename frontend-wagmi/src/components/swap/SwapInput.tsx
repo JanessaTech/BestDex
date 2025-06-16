2d64fcf136a4b71ff93e3fe92fc5e4df710a9b55
Janessa <lvlajzhao1983@gmail.com>
2025-06-16 13:05:03 +0800

    layout for swap input

--- frontend-wagmi/src/components/swap/SwapInput.tsx
+++ frontend-wagmi/src/components/swap/SwapInput.tsx
@@ -10,10 +10,24 @@ const SwapInput: React.FC<SwapInputProps> = ({hidden, onChange}) => {
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setInput(e.target.value)
     }
+
     return (
         <div className={`relative w-3/5 h-full box-border bg-zinc-900 border-zinc-700 border-[1px] rounded-r-md focus:border-pink-600 ${hidden ? 'hidden' : ''}`}>
             <input className={`bg-inherit px-3 box-border ${String(input).length > 10 ? 'mt-2 w-full' : 'mt-2 md:mt-5 w-full md:w-36'}`} 
-            onChange={handleInputChange}></input>
+            onChange={handleInputChange}
+            onKeyDown={(event) => {
+                const allow =  (event?.key === 'Backspace' || event?.key === 'Delete')
+                            || (input === '' && event?.key >= '0' && event?.key <= '9')
+                            || (input === '0' && event?.key === '.')
+                            || (/^[1-9]\d{0,20}$/.test(input) && event?.key >= '0' && event?.key <= '9')
+                            || (/^[1-9]\d{0,20}$/.test(input) && event?.key === '.')
+                            || (/^[1-9]\d{0,20}.\d{0,20}$/.test(input) && event?.key >= '0' && event?.key <= '9')
+                            || (/^0.\d{0,20}$/.test(input) && event?.key >= '0' && event?.key <= '9')
+                if (!allow) {
+                    event.preventDefault(); 
+                }
+            }}
+            />
             <div className={`absolute text-zinc-400 left-2 top-9 text-xs w-24 md:w-28 truncate ${String(input).length > 10 ? '' : 'md:left-32 md:top-6 '} ${String(input).length > 0 ? '' : 'hidden'}`}>
                 <span>=$56894442346664</span>
             </div>
