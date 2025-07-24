e851d8f14e28ad4628a66e1406e796cd50a79451
Janessa <lvlajzhao1983@gmail.com>
2025-07-24 15:26:37 +0800

    verify res for fromReadableAmount

--- uniswap_v3_TS-integration/scripts/tmp.ts
+++ uniswap_v3_TS-integration/scripts/tmp.ts
@@ -1,10 +1,32 @@
 import { ethers, providers, BigNumber} from 'ethers'
 import { Decimal } from 'decimal.js';
+import JSBI from 'jsbi'
+
+function fromReadableAmount(amount: number, decimals: number): JSBI {
+    const extraDigits = Math.pow(10, countDecimals(amount))
+    const adjustedAmount = amount * extraDigits
+    return JSBI.divide(
+      JSBI.multiply(
+        JSBI.BigInt(adjustedAmount),
+        JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(decimals))
+      ),
+      JSBI.BigInt(extraDigits)
+    )
+  }
+
+function countDecimals(x: number) {
+    if (Math.floor(x) === x) {
+        return 0
+    }
+    return x.toString().split('.')[1].length || 0
+}
 
 function test() {
-    const result = new Decimal(97000).times(new Decimal(5136211410)).div('1000000000000000000').toDecimalPlaces(18, Decimal.ROUND_HALF_UP).toString()
-    console.log(result)
+    const amount = '1', decimal = 5
+    const res = fromReadableAmount(Number(amount), decimal).toString()
+    console.log(res)
 }
+
 test()
 
 // npx ts-node .\scripts\tmp.ts 
