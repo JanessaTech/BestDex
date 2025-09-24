9a70dcdc80e8f7b664d84340532b045c2ba363b9
Janessa <lvlajzhao1983@gmail.com>
2025-09-24 15:03:07 +0800

    refactor codes

--- frontend-next13-wagmi/src/components/swap/SwapApproveStep.tsx
+++ frontend-next13-wagmi/src/components/swap/SwapApproveStep.tsx
@@ -8,8 +8,8 @@ import { useWriteContract} from 'wagmi'
 import { ERC20 } from "@/config/abis"
 import { memo } from "react";
 import { fromReadableAmount3 } from "@/lib/utils";
+import { V3_SWAP_ROUTER_ADDRESS } from "@/config/constants";
 
-const V3_SWAP_ROUTER_ADDRESS = '0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45'
 type SwapApproveStepProps = {
     tokenFrom: TokenType;
     approveAmount: string;
@@ -35,39 +35,42 @@ const SwapApproveStep: React.FC<SwapApproveStepProps> = ({tokenFrom, approveAmou
     useEffect(() => {
         let timer = undefined
         if (isSuccess) {
-            console.log('it will goNext in 1000 milliseconds')
+            console.log('[SwapApproveStep] it will goNext in 1000 milliseconds')
             timer = setTimeout(() => {goNext()}, 1000)
         }
         return () => {
             if (timer) clearTimeout(timer)
         }
     }, [isSuccess])
+
+    console.log('[SwapApproveStep] ======= Latest state =======')
     console.log('[SwapApproveStep] isSuccess=', isSuccess, ' isPending=', isPending)
-    console.log('[SwapApproveStep] Approve tx hash =', hash)
+    console.log('[SwapApproveStep] hash =', hash)
+    console.log('[SwapApproveStep] error =', error)
     
     return (
         <div className="flex justify-between items-center">
             <div className="flex items-center relative">
                 <div className={`size-6 border-[1px] rounded-full border-pink-600 border-t-transparent animate-spin absolute ${isPending ? '' : 'hidden'}`}/>
                 <SVGLockOpen className="text-white size-4 ml-[4px]"/>
-                <div className={`text-xs pl-4 ${isPending 
-                                                ? 'text-pink-600' 
-                                                : isSuccess
-                                                    ? 'text-zinc-400'
-                                                    : 'text-red-600'}`}>{isPending ? 'Approve in wallet' 
-                                                                                      : isSuccess
-                                                                                           ? 'Approved'
-                                                                                           : 'Failed to approve'}</div>
+                <div className={`text-xs pl-4 ${isSuccess 
+                                                ? 'text-zinc-400' 
+                                                : error
+                                                    ? 'text-red-600'
+                                                    : 'text-pink-600'}`}>{isSuccess ? 'Approved' 
+                                                                                      : error
+                                                                                           ? 'Failed to approve'
+                                                                                           : `Approve ${tokenFrom.symbol} in wallet`}</div>
             </div>
             <div>
                 {
-                    isPending
-                    ? <></>
-                    : isSuccess
-                        ? <SVGCheck className="size-4 text-green-600 mx-3"/>
-                        : <ToolTipHelper content={<div className="w-80">{error?.message}</div>}>
+                    isSuccess
+                    ? <SVGCheck className="size-4 text-green-600 mx-3"/>
+                    : error
+                        ? <ToolTipHelper content={<div className="w-80">{error?.message}</div>}>
                             <SVGXCircle className="size-5 text-red-600 bg-inherit rounded-full cursor-pointer mx-3"/>
                           </ToolTipHelper>
+                        : <></>
                 }  
             </div>
         </div>
