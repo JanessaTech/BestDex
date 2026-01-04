c725542ccc50441e2aaea55bbea907cf3e7b4edd
Janessa <lvlajzhao1983@gmail.com>
2026-01-04 16:46:46 +0800

    support for multiple chain

--- frontend-next13-wagmi/src/config/wagmi.ts
+++ frontend-next13-wagmi/src/config/wagmi.ts
@@ -4,9 +4,10 @@ import { getDefaultConfig , Chain} from '@rainbow-me/rainbowkit';
 import {
     mainnet,
     bsc,
-    polygon,
+    base,
     arbitrum,
     sepolia,
+    baseSepolia,
     hardhat
   } from 'wagmi/chains';
   import {http } from "wagmi";
@@ -34,23 +35,30 @@ import {
       iconUrl: '/imgs/networks/bnbsmartchain.png',
     },
     {
+      ...base,
+      iconUrl: '/imgs/networks/base.png',
+    },
+    {
       ...arbitrum,
       iconUrl: '/imgs/networks/arbitrumone.png',
     },
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [{...sepolia, iconUrl: '/imgs/networks/sepolia.png'}, 
-                                                              {...hardhat, iconUrl: '/imgs/networks/hardhat.png', contracts: {multicall3: {address: '0xca11bde05977b3631167028862be2a173976ca11' as `0x${string}`, blockCreated: 14353601}}}] 
-                                                           : [])
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [{...sepolia, iconUrl: '/imgs/networks/sepolia.png'},
+                                                              {...baseSepolia, iconUrl: '/imgs/networks/bnbsmartchaintestnet.png'}
+                                                              ] 
+                                                           : []),
+    ...(process.env.NEXT_PUBLIC_ENABLE_LOCALS === 'true' ? [{...hardhat, iconUrl: '/imgs/networks/hardhat.png', contracts: {multicall3: {address: '0xca11bde05977b3631167028862be2a173976ca11' as `0x${string}`, blockCreated: 14353601}}}] : [])
   ]
 
-
-export const chainUrls = { // it should be consistent with the variable chains defined above
+const chainUrls = { // it should be consistent with the variable chains defined above
   [mainnet.id]: `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
   [bsc.id]: `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
+  [base.id]: `https://base-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
   [arbitrum.id]: `https://arb-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
   [sepolia.id]: `https://eth-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
+  [baseSepolia.id]: `https://base-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
   [hardhat.id]: 'http://127.0.0.1:8545'
 }
-export const myTransports = Object.fromEntries(
+const myTransports = Object.fromEntries(
   Object.entries(chainUrls).map(([chainId, url]) => [chainId, http(url)])
 );
 export const config = getDefaultConfig({
