ca4fb5bbc40de0c597c0ecc52bd7ed6b50ee04c9
Janessa <lvlajzhao1983@gmail.com>
2026-01-08 21:49:37 +0800

    fix bug: there is an error when there are more than 3 types of chains to get price

--- backend/infra/utils/Token.ts
+++ backend/infra/utils/Token.ts
@@ -17,19 +17,24 @@ export const getTokenPriceFromAlchemy = async () => {
         'Authorization': `Bearer ${process.env.PUBLIC_ALCHEMY_ID}`,
     }
 
-    const addresses: any = []
-    tokenList.filter((chain) => NETWORK_CONSTANTS.MAINNET.includes(chain.network_enum as MAINNET_ENUM)).forEach((chain) => chain.tokens.forEach((token) => addresses.push({network: chain.network_enum, address: token.address})))
-    const data = {addresses: addresses}
-    //console.log('data=', data)
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(data)
-        });
+    const allAddresses: any = []
+    tokenList.filter((chain) => NETWORK_CONSTANTS.MAINNET.includes(chain.network_enum as MAINNET_ENUM)).forEach((chain) => chain.tokens.forEach((token) => allAddresses.push({network: chain.network_enum, address: token.address})))
+    const res: {data: any[]} = {data: []}
+    for (let i = 0; i < allAddresses.length; i += 3) {
+        const data = {addresses: allAddresses.slice(i, i + 3)}
+        //console.log('data=', data)
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(data)
+            });
 
-    if (!response.ok) {
-    throw new Error('response is not ok to get token price');
+        if (!response.ok) {
+        throw new Error('response is not ok to get token price');
+        }
+        const payload = await response.json() as {data: any[]};
+        res.data.push(...payload.data)
     }
-    const payload = await response.json()
-    return payload
+    
+    return res
 }
