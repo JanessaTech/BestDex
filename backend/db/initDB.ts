275f8de45a59b7e77a5c7f680029671338750b9d
Janessa <lvlajzhao1983@gmail.com>
2026-01-20 13:48:04 +0800

    debug for db url

--- backend/db/initDB.ts
+++ backend/db/initDB.ts
@@ -5,6 +5,7 @@ import getConfig from '../config/configuration'
 const config = getConfig()
 //const dbURI = `mongodb://${config.database.host}:${config.database.port}/${config.database.dataBaseName}`
 const dbURI = `mongodb://${config.database.username}:${config.database.password}@127.0.0.1:${config.database.port}/${config.database.dataBaseName}?authSource=${config.database.dataBaseName}`
+logger.debug('dbURI =', dbURI)
 const options = {
     autoIndex: true,
     minPoolSize: config.database.minPoolSize, 
