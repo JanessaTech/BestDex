7d312d82f39c5e524b9eb67a228d56ed11f77c57
Janessa <lvlajzhao1983@gmail.com>
2026-02-21 21:56:43 +0800

    Add unit test for TransactionController

--- backend/test/controllers/TransactionController.test.ts
+++ backend/test/controllers/TransactionController.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../../app'
 import transactionService from '../../services/TransactionService';
 import messageHelper from '../../helpers/internationalization/messageHelper';
-import { TokenType } from '../../controllers/types';
+import { PaginationReturnType, TokenType, TransactionInfoType } from '../../controllers/types';
 import getConfig from '../../config/configuration';
 
 const config = getConfig()
@@ -44,27 +44,43 @@ describe('TransactionController', () => {
         jest.clearAllMocks()
     })
 
+    const chainId = 1
+    const token0 = {chainId: chainId, name: 'token0', symbol: 'symbol', decimal: 18, alias: 'token0', address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'} as TokenType
+    const token1 = {chainId: chainId, name: 'token1', symbol: 'symbol', decimal: 18, alias: 'token1', address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'} as TokenType
+
     describe('POST /transactions', () => {
         const endpoint = apiPrefix + '/transactions'
-        console.log('endpoint=', endpoint)
+        const from = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'
         const validBody = {
-            chainId: '1',
+            chainId: chainId,
             tx: '0x1234433333',
-            token0: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-            token1: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+            token0: token0.address,
+            token1: token1.address,
             txType: 'Mint',
             amount0: '100',
             amount1: '200',
             usd: '300',
-            from: '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266',
+            from: from,
+        }
+
+        const invalidBody = {
+            chainId: chainId,
+            tx: '0x1234433333',
+            token0: '0xinvalidhash',
+            token1: token1.address,
+            txType: 'Mint',
+            amount0: '100',
+            amount1: '200',
+            usd: '300',
+            from: from,
         }
         it('should create a new transaction successfully', async () => {
             const createdTx = {
                 id: 1,
-                chainId: Number(validBody.chainId),
+                chainId: chainId,
                 tx: validBody.tx,
-                token0: {chainId: Number(validBody.chainId), name: 'token0', symbol: 'symbol', decimal: 18, alias: 'token0', address: validBody.token0} as TokenType,
-                token1: {chainId: Number(validBody.chainId), name: 'token1', symbol: 'symbol', decimal: 18, alias: 'token1', address: validBody.token1} as TokenType,
+                token0: token0,
+                token1: token1,
                 txType: validBody.txType,
                 amount0: validBody.amount0,
                 amount1: validBody.amount1,
@@ -83,9 +99,87 @@ describe('TransactionController', () => {
             expect(mockedTransactionService.create).toHaveBeenCalledTimes(1)
             expect(response.body.success).toEqual(true)
         })
+        it('failed to create a new transaction', async function() {
+            const createdTx = {
+                id: chainId,
+                chainId: chainId,
+                tx: invalidBody.tx,
+                token0: token0,
+                token1: token0,
+                txType: invalidBody.txType,
+                amount0: invalidBody.amount0,
+                amount1: invalidBody.amount1,
+                usd: invalidBody.usd,
+                from: invalidBody.from,
+                createdAt: new Date()
+            }
+            mockedTransactionService.create.mockResolvedValue(createdTx)
+            mockedMessageHelper.getMessage.mockReturnValue('Transaction created successfully');
+
+            const response = await request(app)
+                        .post(endpoint)
+                        .send(invalidBody)
+                        .expect(400)
+            expect(response.body.success).toEqual(false)
+        })
     })
 
     describe('GET /transactions/:chainId', () => {
+        const baseUrl = apiPrefix + `/transactions/${chainId}`
+        const from = '0xf39fd6e51aad88f6f4ce6ab8827279cfffB92266'.toLocaleLowerCase()
+        it('should return transactions list with pagination',async function() {
+            const page = 1
+            const pageSize = 3
+            const totalPages = 1
+            const totalResults = 2
 
+            const mockedTxs: TransactionInfoType[] = [
+                {id: 1, chainId: chainId, tx: "0x123", token0: token0, token1: token0, txType: 'Mint', amount0: '100', amount1: '200', usd: '300', from: from, createdAt: new Date()},
+                {id: 2, chainId: chainId, tx: "0x456", token0: token0, token1: token0, txType: 'Mint', amount0: '100', amount1: '200', usd: '300', from: from, createdAt: new Date()},
+                {id: 3, chainId: chainId, tx: "0x789", token0: token0, token1: token0, txType: 'Mint', amount0: '100', amount1: '200', usd: '300', from: from, createdAt: new Date()},
+            ]
+            const returnedPagination = {
+                results: mockedTxs,
+                page: page,
+                pageSize:pageSize,
+                totalPages: totalPages,
+                totalResults: totalResults
+            } as PaginationReturnType
+            const emptyReturnedPagination = {
+                results: [],
+                page: page,
+                pageSize:pageSize,
+                totalPages: totalPages,
+                totalResults: totalResults
+            } as PaginationReturnType
+
+            mockedTransactionService.getTransactions.mockImplementation(
+                async (_chainId, _from, _page, _pageSize) => {
+                    if (
+                        _chainId === chainId &&
+                        _from === from &&
+                        _page === page &&
+                        _pageSize === pageSize
+                      ) {
+                        return returnedPagination
+                      } else {
+                        return emptyReturnedPagination
+                      }
+                }
+            )
+
+            const response = await request(app)
+                        .get(baseUrl)
+                        .query({
+                            from : from,
+                            page: page,
+                            pageSize: pageSize
+                        })
+                        .expect(200)
+            expect(response.body.data.results).toHaveLength(3)
+            expect(mockedTransactionService.getTransactions).toHaveBeenCalledWith(
+                chainId, from, page, pageSize
+            )
+        })
     })
 })
